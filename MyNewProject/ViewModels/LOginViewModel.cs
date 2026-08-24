86d7c5cdfb375c3845159d804e752dc3575bc1ac
m0n5ter <valerii.karkachev@gmail.com>
2026-08-24 21:48:58 +0300

    Add project files.

---
+++ MyNewProject/ViewModels/LOginViewModel.cs
@@ -0,0 +1,75 @@
+﻿namespace MyNewProject.ViewModels;
+
+internal class LOginViewModel : ViewModelBase
+{
+    private bool _loggedIn;
+    private string _password;
+    private string _userName;
+    private bool _isLoggingIn;
+
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            if (SetField(ref _userName, value))
+            {
+                OnPropertyChanged(nameof(HelloText));
+                LoginCommand.RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            if (SetField(ref _password, value))
+            {
+                OnPropertyChanged(nameof(HelloText));
+                LoginCommand.RaiseCanExecuteChanged();
+            }
+        }
+    }
+
+    public string HelloText => $"Hello {UserName}! Password: {Password}";
+
+    public bool LoggedIn
+    {
+        get => _loggedIn;
+        set => SetField(ref _loggedIn, value);
+    }
+
+    public UiCommand LoginCommand { get; }
+
+    public LOginViewModel()
+    {
+        LoginCommand = new UiCommand(async () => await Login(), () => !string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password));
+    }
+
+    private async Task Login()
+    {
+        try
+        {
+            IsLoggingIn = true;
+            await Task.Delay(3000);
+            LoggedIn = UserName == "admin" && Password == "123";
+            UserName = Password = string.Empty;
+        }
+        catch
+        {
+            // TODO log or something
+        }
+        finally
+        {
+            IsLoggingIn = false;
+        }
+    }
+
+    public bool IsLoggingIn
+    {
+        get => _isLoggingIn;
+        set => SetField(ref _isLoggingIn, value);
+    }
+}
---
+++ MyNewProject/ViewModels/UiCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+
+namespace MyNewProject.ViewModels;
+
+internal class UiCommand : UiCommand<object>
+{
+    public UiCommand(Action action, Func<bool>? canExecute = null) : base(_ => action(), canExecute== null? null: _ => canExecute())
+    {
+    }
+}
+
+internal class UiCommand<TParameter>: ICommand where TParameter : class
+{
+    private readonly Action<TParameter?> _action;
+    private readonly Predicate<TParameter?>? _canExecute;
+
+    public UiCommand(Action<TParameter?> action, Predicate<TParameter?>? canExecute = null)
+    {
+        _action = action;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_canExecute == null)
+            return true;
+
+        return _canExecute(parameter as TParameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _action(parameter as TParameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
